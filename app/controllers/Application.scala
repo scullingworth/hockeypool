7edbc44f65f79871e8d4cad6690b026726bfc534
Steve Cullingworth <steve@cullingworth.com>
2015-10-10 19:42:38 -0700

    Add Postgres support

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,32 @@
 package controllers
 
 import play.api._
+import play.api.db.DB
 import play.api.mvc._
 
 class Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def db = Action {
+    var out = ""
+    val conn = DB.getConnection()
+    try {
+      val stmt = conn.createStatement
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)")
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())")
+
+      val rs = stmt.executeQuery("SELECT tick FROM ticks")
+
+      while (rs.next) {
+        out += "Read from DB: " + rs.getTimestamp("tick") + "\n"
+      }
+    } finally {
+      conn.close()
+    }
+    Ok(out)
   }
 
 }
